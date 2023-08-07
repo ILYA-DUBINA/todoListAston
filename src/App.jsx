f89a2db18b787219abbb8ed8a2747f5a6f9d3903
Ilya <dinlion@yandex.ru>
2023-08-07 14:37:59 +0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -169,8 +169,9 @@ export default class App extends Component {
   componentDidMount() {
     let arrayStorage = JSON.parse(localStorage.getItem('arr'));
     this.setState({
-      arrayElements:
-        arrayStorage.length === 0 ? this.state.arrayElements : arrayStorage,
+      arrayElements: !arrayStorage.length
+        ? this.state.arrayElements
+        : arrayStorage,
     });
   }
   componentDidUpdate() {
